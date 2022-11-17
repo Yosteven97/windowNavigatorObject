84b106eaa67bc41c4cb20c5bbee2a60e46ca9b10
Yosteven <yosteven.yosteven@adira.co.id>
2022-11-17 11:33:43 +0700

    window navigator object

---
+++ index.js
@@ -0,0 +1,10 @@
+//Window Navigator Object
+
+let browserCodeName = navigator.appCodeName
+let browserName = navigator.appName
+let browserVersion = navigator.appVersion
+let cookiesEnabled = navigator.cookieEnabled
+let browserLanguage = navigator.language
+let online = navigator.onLine
+let platform = navigator.platform
+let userAgentHeader = navigator.userAgent
